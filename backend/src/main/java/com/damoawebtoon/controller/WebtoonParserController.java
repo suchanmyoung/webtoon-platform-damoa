77fa06d3460e2c5768f8148232331648891d216f
suchanmyung <aud248@naver.com>
2022-06-08 23:31:22 +0900

    feat : 레진코믹스 컨트롤러 추가

--- backend/src/main/java/com/damoawebtoon/controller/WebtoonParserController.java
+++ backend/src/main/java/com/damoawebtoon/controller/WebtoonParserController.java
@@ -5,6 +5,7 @@ import com.damoawebtoon.service.WebtoonParserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,19 +18,25 @@ public class WebtoonParserController {
 
     private final WebtoonParserService webtoonParserService;
 
-    String naverWebtoonURL = "https://comic.naver.com/webtoon/weekday";
-    String kakaoWebtoonURL = "https://webtoon.kakao.com/original-webtoon";
+    private String naverWebtoonURL = "https://comic.naver.com/webtoon/weekday";
+    private String kakaoWebtoonURL = "https://webtoon.kakao.com/original-webtoon";
+    private String lezhinWebtoonURL = "https://www.lezhin.com/ko/scheduled";
 
-
-    @RequestMapping("/naver")
+    @GetMapping("/naver")
     public ResponseEntity<List<Webtoon>> naverWebtoonParse(){
         List<Webtoon> webtoons = webtoonParserService.parse(naverWebtoonURL);
         return ResponseEntity.ok(webtoons);
     }
 
-    @RequestMapping("/kakao")
+    @GetMapping("/kakao")
     public ResponseEntity<List<Webtoon>> kakaoWebtoonParse(){
         List<Webtoon> webtoons = webtoonParserService.parse(kakaoWebtoonURL);
+        return ResponseEntity.ok(webtoons);
+    }
+
+    @GetMapping("/lezhin")
+    public ResponseEntity<List<Webtoon>> lezhinWebtoonParse(){
+        List<Webtoon> webtoons = webtoonParserService.parse(lezhinWebtoonURL);
         return ResponseEntity.ok(webtoons);
     }
 }
